ee5c3ddb5c8c0f48cc71c2df3e6e490980f51142
fcbdata <113846514+fcbdata@users.noreply.github.com>
2022-09-23 20:52:34 +0200

    Update int.py

--- int.py
+++ int.py
@@ -18,8 +18,6 @@ def get_data():
 
 df = get_data()
 
-st.dataframe(df.style.format(subset=['interes', 'interes_acumulado'], formatter="{:.2f}))
-
 # app sidebar
 st.sidebar.header('📖 Definición de Interés')
 st.sidebar.markdown(
