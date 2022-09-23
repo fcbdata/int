e1bb776f6fe7b46371f73223d627ab48189b0c22
fcbdata <113846514+fcbdata@users.noreply.github.com>
2022-09-23 19:33:01 +0200

    Update int.py

--- int.py
+++ int.py
@@ -148,12 +148,12 @@ st.altair_chart(c, use_container_width=False)
 # EVOLUCIÓN %CR
 # create a selection that chooses the nearest point & selects based on x-value
 nearest = alt.selection(type='single', nearest=True, on='mouseover',
-                        fields=['%cr'], empty='none')
+                        fields=['cr'], empty='none')
 
 # the basic line
 line = alt.Chart(filtered_df).mark_line(interpolate='basis').encode(
     x='antelacion:Q',
-    y='%cr:Q',
+    y='cr:Q',
     color='id:N'
 )
 
@@ -172,7 +172,7 @@ points = line.mark_point().encode(
 
 # draw text labels near the points and highlight based on selection
 text = line.mark_text(align='left', dx=5, dy=-5).encode(
-    text=alt.condition(nearest, '%cr:Q', alt.value(' '))
+    text=alt.condition(nearest, 'cr:Q', alt.value(' '))
 )
 
 # draw a rule at the location of the selection
